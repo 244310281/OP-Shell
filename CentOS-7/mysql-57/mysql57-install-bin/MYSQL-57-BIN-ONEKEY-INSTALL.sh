8c5f99559827cca8d07f4b8f4a6e39fdb993e211
e <1214966109@qq.com>
2019-09-30 14:45:43 +0800

    新增MySQL 5.7 二进制安装脚本

--- CentOS-7/mysql-57/mysql57-install-bin/MYSQL-57-BIN-ONEKEY-INSTALL.sh
+++ CentOS-7/mysql-57/mysql57-install-bin/MYSQL-57-BIN-ONEKEY-INSTALL.sh
@@ -4,16 +4,16 @@
 # 脚本信息
 #############################################################################################
 # 作者：Dylan <1214966109@qq.com>   
-# 用途：一键 Nginx 编译安装 / 初始化 / 调优
+# 用途：一键安装配置 MySQL 5.7 / 初始化 / 调优
 # 系统：CentOS 7.x
-# 时间：2019-07-27 14:45
+# 时间：2019-09-28 17:24
 #############################################################################################
 
 #############################################################################################
 # 脚本规范说明
 #############################################################################################   
 # 1. 安装包名称，版本都会以单独的变量单独定义组合，且抽离在脚本最前面便于维护
-# 2. 变量 / 函数名称 / 数组都将使用全大写加下划线的形式，如：PLG_NGX_ECHO
+# 2. 变量 / 函数名称 / 数组都将使用全大写加下划线的形式，如：PLG_MYSQL_ECHO
 # 3. 各个分类都将以单独的名称开始
 #     普通变量：VAR_XXX_XXX
 #     包名变量：PKG_XXX
@@ -28,19 +28,10 @@
 #############################################################################################
 # 脚本功能说明
 #############################################################################################   
-# 1. 本脚本负责 nginx 一键编译安装 / 内核调优 / 配置调优
-# 2. nginx 默认所以目录均在 /data 目录下，可根据自己需求修改
-# 3. 在 nginx 的 conf/templates 目录中提供了常用的大部分 nginx 配置示例可供参考
+# 1. 本脚本负责 MySQL 5.7 二进制一键安装 / 初始化 / 配置调优
+# 2. MySQL 默认所以目录均在 /data 目录下，可根据自己需求修改
 # 4. 目录解析（默认）：
 #     /data/services/nginx：默认安装目录
-#     /data/logs/nginx：默认日志路径
-#     /data/backup/nginx：默认备份路径
-#     /data/www/nginx：默认项目目录
-#     /data/services/nginx/conf/users：默认用户认证文件目录
-#     /data/services/nginx/conf/templates：默认配置示例目录
-#     /data/services/nginx/conf/vhosts：默认虚拟主机目录
-#     /data/services/nginx/conf/tcp：默认TCP代理配置目录
-#     /data/services/nginx/conf/certs：默认 ssl 证书目录
 #############################################################################################
 
 #############################################################################################
@@ -57,105 +48,44 @@ LINE='--------------------------------------------------------------------------
 
 # 基础路径 / 安装路径 / 配置路径 / 日志路径 / 项目路径 / 备份路径
 PATH_BASE="/data"
-PATH_NGX_BASE_INSTALL="${PATH_BASE}/services/nginx"
-PATH_NGX_CONFIG="${PATH_NGX_BASE_INSTALL}/conf"
-PATH_NGX_LOG="${PATH_BASE}/logs/nginx"
-PATH_NGX_WEB="${PATH_BASE}/www/nginx"
-PATH_NGX_BACKUP="${PATH_BASE}/backup/nginx"
+PATH_MYSQL_BASE_INSTALL="${PATH_BASE}/services/mysql"
+PATH_MYSQL_DATA="${PATH_BASE}/data/mysql"
+PATH_MYSQL_LOG="${PATH_BASE}/logs/mysql"
+PATH_MYSQL_BACKUP="${PATH_BASE}/backup/mysql"
 
 # 安装包 / 模板路径
 PATH_PWD=$(/usr/bin/pwd)
 PATH_PKG="${PATH_PWD}/packages"
 PATH_INIT_CONFIG="${PATH_PWD}/conf"
-PATH_INIT_CONFIG_TEMPLATE="${PATH_INIT_CONFIG}/templates"
-PATH_INIT_CONFIG_YUM="${PATH_PWD}/../common/conf/yum"
-PATH_INIT_CONFIG_SYSTEM="${PATH_PWD}/../common/conf/system"
+PATH_INIT_CONFIG_SYSTEM="${PATH_PWD}/../../common/conf/system"
 
 # 进程用户
-USER_PROCESS='root'
+MYSQL_PROCESS_USER='mysql'
+
+# 服务端口
+MYSQL_PORT='3306'
+
+# 数据库 ID
+MYSQL_SERVER_ID='100'
+
+# 分配内存，一般为服务器内存 80%
+MYSQL_MEMORY='2G'
+
+# 初始密码
+MYSQL_INIT_PASSWORD='123456'
 
 # 安装包下载地址
-PKG_DOWNLOAD_URL='源码包下载链接：https://pan.baidu.com/s/1IpRtZgYFb-Kf71ED3pBEwQ【提取码：2k6t】'
+PKG_DOWNLOAD_URL='下载地址：https://cdn.mysql.com/archives/mysql-5.7/mysql-5.7.25-linux-glibc2.12-x86_64.tar.gz'
 
 # 主程序解压包名称
-PKG_NGX_NAME='nginx-1.16.0'
-
-# 依赖库解压包名称
-PKG_DEP_OPENSSL_NAME='openssl-1.1.1c'
-PKG_DEP_PCRE_NAME='pcre-8.43'
-PKG_DEP_ZLIB_NAME='zlib-1.2.11'
-
-# 插件解压包名称
-PKG_PLG_ECHO_NAME='echo-nginx-module-master'
-PKG_PLG_CACHE_NAME='ngx_cache_purge-master'
-PKG_PLG_UPLOAD_NAME='nginx-upload-module-master'
-PKG_PLG_FAIR_NAME='nginx-upstream-fair-master'
-PKG_PLG_FANCYINDEX_NAME='ngx-fancyindex-master'
-PKG_PLG_PROXY_NAME='ngx_http_proxy_connect_module-master'
-PKG_PLG_CODE_NAME='ngx_http_status_code_counter-master'
+PKG_MYSQL_NAME='mysql-5.7.25-linux-glibc2.12-x86_64'
 
 # 安装包最终名称：tar.gz
-PKG_NGX=${PKG_NGX_NAME}.tar.gz
-PKG_DEP_OPENSSL=${PKG_DEP_OPENSSL_NAME}.tar.gz
-PKG_DEP_PCRE=${PKG_DEP_PCRE_NAME}.tar.gz
-PKG_DEP_ZLIB=${PKG_DEP_ZLIB_NAME}.tar.gz
-
-# 安装包最终名称：zip
-PKG_PLG_ECHO=${PKG_PLG_ECHO_NAME}.zip
-PKG_PLG_CACHE=${PKG_PLG_CACHE_NAME}.zip
-PKG_PLG_UPLOAD=${PKG_PLG_UPLOAD_NAME}.zip
-PKG_PLG_FAIR=${PKG_PLG_FAIR_NAME}.zip
-PKG_PLG_FANCYINDEX=${PKG_PLG_FANCYINDEX_NAME}.zip
-PKG_PLG_PROXY=${PKG_PLG_PROXY_NAME}.zip
-PKG_PLG_CODE=${PKG_PLG_CODE_NAME}.zip
-
-# 安装包数组
-ARR_PKG=(
-${PKG_NGX}
-${PKG_DEP_OPENSSL}
-${PKG_DEP_PCRE}
-${PKG_DEP_ZLIB}
-${PKG_PLG_ECHO}
-${PKG_PLG_CACHE}
-${PKG_PLG_UPLOAD}
-${PKG_PLG_FAIR}
-${PKG_PLG_FANCYINDEX}
-${PKG_PLG_PROXY}
-${PKG_PLG_CODE}
-)
+PKG_MYSQL=${PKG_MYSQL_NAME}.tar.gz
 
 # tar.gz 包组
 ARR_PKG_TAR=(
-${PKG_NGX}
-${PKG_DEP_OPENSSL}
-${PKG_DEP_PCRE}
-${PKG_DEP_ZLIB}
-)
-
-# zip 包组
-ARR_PKG_ZIP=(
-${PKG_PLG_ECHO}
-${PKG_PLG_CACHE}
-${PKG_PLG_UPLOAD}
-${PKG_PLG_FAIR}
-${PKG_PLG_FANCYINDEX}
-${PKG_PLG_PROXY}
-${PKG_PLG_CODE}
-)
-
-# 解压包数组
-ARR_PKG_NAME=(
-${PKG_NGX_NAME}
-${PKG_DEP_OPENSSL_NAME}
-${PKG_DEP_PCRE_NAME}
-${PKG_DEP_ZLIB_NAME}
-${PKG_PLG_ECHO_NAME}
-${PKG_PLG_CACHE_NAME}
-${PKG_PLG_UPLOAD_NAME}
-${PKG_PLG_FAIR_NAME}
-${PKG_PLG_FANCYINDEX_NAME}
-${PKG_PLG_PROXY_NAME}
-${PKG_PLG_CODE_NAME}
+${PKG_MYSQL}
 )
 
 #############################################################################################
@@ -228,25 +158,14 @@ function FUNC_USER_CHECK() {
 }
 
 #############################################################################################
-# 服务器联网函数
-#############################################################################################
-function FUNC_NETWORK_CHECK() {
-  VAR_PING_NUM=$(/usr/bin/ping -c 3 www.baidu.com | grep 'icmp_seq' | wc -l)
-  if [[ ${VAR_PING_NUM} -eq 0 ]];then
-    FUNC_ECHO_ERROR '网络连接失败，请先配置好网络连接...'
-    exit 1004
-  fi
-}
-
-#############################################################################################
 # 安装检测函数
 #############################################################################################
 function FUNC_INSTALL_CHECK() {
   # 检查安装路径是否已被使用
-  if [[ -d ${PATH_NGX_BASE_INSTALL} ]];then
-    cd ${PATH_NGX_BASE_INSTALL}
+  if [[ -d ${PATH_MYSQL_BASE_INSTALL} ]];then
+    cd ${PATH_MYSQL_BASE_INSTALL}
     if [[ $(ls -l | wc -l) -gt 1 ]];then
-      FUNC_ECHO_RED "安装路径 [${PATH_NGX_BASE_INSTALL}] 下已存在未知文件，请确认无用后删掉再次执行安装!"
+      FUNC_ECHO_RED "安装路径 [${PATH_MYSQL_BASE_INSTALL}] 下已存在未知文件，请确认无用后删掉再次执行安装!"
       exit 1005
     fi
   fi
@@ -315,19 +234,15 @@ function FUNC_INSTALL_INFO() {
     
   # 打印安装信息
   FUNC_ECHO_GREEN ${LINE}
-  FUNC_ECHO_GREEN "Nginx 安装完成，但是未启动，安装信息如下: "
+  FUNC_ECHO_GREEN "MySQL 安装完成，安装信息如下: "
   FUNC_ECHO_GREEN ${LINE}
-  FUNC_ECHO_GREEN "安装路径: ${PATH_NGX_BASE_INSTALL}"
-  FUNC_ECHO_GREEN "日志路径: ${PATH_NGX_LOG}"
-  FUNC_ECHO_GREEN "项目路径: ${PATH_NGX_WEB}"
-  FUNC_ECHO_GREEN "备份路径: ${PATH_NGX_BACKUP}"
-  FUNC_ECHO_GREEN "模板路径: ${PATH_NGX_CONFIG}/templates"
-  FUNC_ECHO_GREEN "认证路径: ${PATH_NGX_CONFIG}/users"
-  FUNC_ECHO_GREEN "证书路径: ${PATH_NGX_CONFIG}/certs"
-  FUNC_ECHO_GREEN "主机路径: ${PATH_NGX_CONFIG}/vhosts"
-  FUNC_ECHO_GREEN "TCP 代理: ${PATH_NGX_CONFIG}/tcp"
-  FUNC_ECHO_GREEN "配置检查: ${PATH_NGX_BASE_INSTALL}/sbin/nginx -t"
-  FUNC_ECHO_GREEN "启动命令: ${PATH_NGX_BASE_INSTALL}/sbin/nginx"
+  FUNC_ECHO_GREEN "安装路径: ${PATH_MYSQL_BASE_INSTALL}"
+  FUNC_ECHO_GREEN "日志路径: ${PATH_MYSQL_LOG}"
+  FUNC_ECHO_GREEN "备份路径: ${PATH_MYSQL_BACKUP}"
+  FUNC_ECHO_GREEN "配置文件: /etc/my.cnf"
+  FUNC_ECHO_GREEN "服务端口: ${MYSQL_PORT}"
+  FUNC_ECHO_GREEN "初始密码: ${MYSQL_INIT_PASSWORD}"
+  FUNC_ECHO_GREEN "启动命令: systemctl start mysqld"
   FUNC_ECHO_GREEN "安装用时：${TIME_USE} 秒"
   FUNC_ECHO_GREEN ${LINE}
 }
@@ -336,22 +251,21 @@ function FUNC_INSTALL_INFO() {
 # 系统调优函数
 #############################################################################################
 function FUNC_SYSTEM_TUNING() {
-  # 创建相关目录
-  mkdir -p ${PATH_NGX_LOG}
-  mkdir -p ${PATH_NGX_WEB}
-  mkdir -p ${PATH_NGX_BACKUP}
-
   # 打开文件数
   if [[ -f ${PATH_INIT_CONFIG_SYSTEM}/limits.conf ]];then
-    mv /etc/security/limits.conf ${PATH_NGX_BACKUP}/limits.conf-$(/usr/bin/date +%F)
+    mv /etc/security/limits.conf /etc/security/limits.conf-$(/usr/bin/date +%F)
     cp ${PATH_INIT_CONFIG_SYSTEM}/limits.conf /etc/security/
   fi
 
   # 内核调优
   if [[ -f ${PATH_INIT_CONFIG_SYSTEM}/sysctl.conf ]];then
-    mv /etc/sysctl.conf ${PATH_NGX_BACKUP}/sysctl.conf-$(/usr/bin/date +%F)
+    mv /etc/sysctl.conf /etc/sysctl.conf-$(/usr/bin/date +%F)
     cp ${PATH_INIT_CONFIG_SYSTEM}/sysctl.conf /etc/
   fi
+
+  # SWAP 优化
+  echo 0 >/proc/sys/vm/swappiness
+  echo 'vm.swappiness = 0' >>/etc/sysctl.conf
 
   # 输出
   FUNC_ECHO_SUCCESS '系统调优参数：'
@@ -359,126 +273,106 @@ function FUNC_SYSTEM_TUNING() {
 }
 
 #############################################################################################
-# 依赖安装函数
-#############################################################################################
-function FUNC_YUM_DEPENDS() {
-  # yum 配置
-  if [[ -f ${PATH_INIT_CONFIG_YUM}/ali.repo ]];then
-    cp ${PATH_INIT_CONFIG_YUM}/ali.repo /etc/yum.repos.d/
-  fi
-
-  if [[ -f ${PATH_INIT_CONFIG_YUM}/epel.repo ]];then
-    cp ${PATH_INIT_CONFIG_YUM}/epel.repo /etc/yum.repos.d/
-  fi
-
-  # 基础依赖安装
-  yum -y install patch gcc gcc-c++ automake autoconf libtool make glibc gd-devel pcre-devel libmcrypt-devel mhash-devel libxslt-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel libevent libevent-devel net-tools httpd-tools zip unzip
-  
-  # 输出
-  if [[ $? -ne 0 ]];then
-    FUNC_ECHO_ERROR 'YUM 安装依赖失败，检查 epel/ali 源配置是否成功！'
-    exit 1007
-  fi
-}
-
-#############################################################################################
 # 编译安装函数
 #############################################################################################
-function FUNC_INSTALL_NGINX() {
+function FUNC_INSTALL_MYSQL() {
+  # 创建相关用户
+  useradd -s /sbin/nologin ${MYSQL_PROCESS_USER}
+
+  # 创建相关目录
+  mkdir -p ${PATH_BASE}/services
+  mkdir -p ${PATH_MYSQL_DATA}
+  mkdir -p ${PATH_MYSQL_BACKUP}
+  mkdir -p ${PATH_MYSQL_LOG}/{bin-log,error-log,relay-log,slow-log}
+
+  # 修改目录权限
+  chown -R ${MYSQL_PROCESS_USER}.${MYSQL_PROCESS_USER} ${PATH_MYSQL_DATA}
+  chown -R ${MYSQL_PROCESS_USER}.${MYSQL_PROCESS_USER} ${PATH_MYSQL_LOG}
+
   cd ${PATH_PKG}
 
   # 解压 tar.gz
   for PKG in ${ARR_PKG_TAR[*]};do
     tar -zxf ${PKG}
     if [[ $? -ne 0 ]];then
-      FUNC_ECHO_ERROR "${PKG} 解压失败，请检查该源码包是否存在问题！"
+      FUNC_ECHO_ERROR "${PKG} 解压失败，请检查该包是否存在问题！"
       exit 1008
     fi
   done
 
-  # 解压 zip
-  for PKG in ${ARR_PKG_ZIP[*]};do
-    unzip ${PKG}
-    if [[ $? -ne 0 ]];then
-      FUNC_ECHO_ERROR "${PKG} 解压失败，请检查该源码包是否存在问题！"
-      exit 1009
-    fi
-  done
-
-  # 隐藏 nginx 信息
-  sed -i 's#"nginx/"#"WEB-SERVER/"#g' ${PKG_NGX_NAME}/src/core/nginx.h
-  sed -i 's#"NGINX"#"WEB-SERVER"#g' ${PKG_NGX_NAME}/src/core/nginx.h
-  sed -i 's#"Server: nginx"#"Server: WEB-SERVER"#g' ${PKG_NGX_NAME}/src/http/ngx_http_header_filter_module.c
-  sed -i 's#"<hr><center>nginx</center>" CRLF##g' ${PKG_NGX_NAME}/src/http/ngx_http_special_response.c
-
-  # 打补丁
-  sed -i 's/default_port/no_port/g' ${PKG_PLG_FAIR_NAME}/ngx_http_upstream_fair_module.c
-  cd ${PKG_NGX_NAME}
-  patch -p1 < ${PATH_PKG}/${PKG_PLG_PROXY_NAME}/patch/proxy_connect_rewrite_101504.patch
-
-  # 编译
-  ./configure --prefix=${PATH_NGX_BASE_INSTALL} \
-  --with-http_stub_status_module \
-  --with-http_gzip_static_module \
-  --with-http_secure_link_module \
-  --with-http_flv_module \
-  --with-http_ssl_module \
-  --with-http_mp4_module \
-  --with-stream \
-  --with-http_realip_module \
-  --with-http_v2_module \
-  --with-http_sub_module \
-  --with-http_image_filter_module \
-  --with-pcre=../${PKG_DEP_PCRE_NAME} \
-  --with-openssl=../${PKG_DEP_OPENSSL_NAME} \
-  --with-zlib=../${PKG_DEP_ZLIB_NAME} \
-  --add-module=../${PKG_PLG_UPLOAD_NAME} \
-  --add-module=../${PKG_PLG_FAIR_NAME} \
-  --add-module=../${PKG_PLG_CACHE_NAME} \
-  --add-module=../${PKG_PLG_FANCYINDEX_NAME} \
-  --add-module=../${PKG_PLG_ECHO_NAME} \
-  --add-module=../${PKG_PLG_PROXY_NAME} \
-  --add-module=../${PKG_PLG_CODE_NAME}
+  # 移动包
+  mv ${PKG_MYSQL_NAME} ${PATH_MYSQL_BASE_INSTALL}
 
   if [[ $? -ne 0 ]];then
-    FUNC_ECHO_ERROR '编译参数检查失败！'
+    FUNC_ECHO_ERROR 'MySQL 解压包移动位置失败！'
     exit 1010
   fi
 
-  make -j4 && make install
-  if [[ $? -ne 0 ]];then
-    FUNC_ECHO_ERROR '编译安装失败！'
-    exit 1010
-  fi
+  # 添加环境变量
+  echo "export PATH=\$PATH:${PATH_BASE}/services/mysql/bin" >> /etc/profile
+  source /etc/profile
+  mysql -V
 }
 
 #############################################################################################
 # 初始化配置函数
 #############################################################################################
-function FUNC_INIT_NGINX() {
-  # 创建配置目录
-  cd ${PATH_NGX_CONFIG} && mkdir -p {users,vhosts,certs,tcp,templates}
+function FUNC_INIT_MYSQL() {
+  # 拷贝主配置文件
+  mv /etc/my.cnf /etc/my.cnf-$(/usr/bin/date +%F)
+  cp ${PATH_INIT_CONFIG}/my.cnf /etc/
+
+  # 修改配置
+  sed -i "s#MYSQL_PORT#${MYSQL_PORT}#g" /etc/my.cnf
+  sed -i "s#MYSQL_SERVER_ID#${MYSQL_SERVER_ID}#g" /etc/my.cnf
+  sed -i "s#MYSQL_PATH#${PATH_BASE}#g" /etc/my.cnf
+  sed -i "s#MYSQL_MEMORY#${MYSQL_MEMORY}#g" /etc/my.cnf
+
+  # 初始化数据库
+  ${PATH_MYSQL_BASE_INSTALL}/bin/mysqld --initialize-insecure --user=${MYSQL_PROCESS_USER} --datadir=${PATH_MYSQL_DATA} --basedir=${PATH_MYSQL_BASE_INSTALL}
   
   if [[ $? -ne 0 ]];then
-    FUNC_ECHO_ERROR '配置文件目录初始化失败，请检查！'
-    exit 1010
+    FUNC_ECHO_ERROR 'MySQL 数据初始化失败！'
+    exit 1011
   fi
 
-  # 拷贝主配置文件
-  mv ${PATH_NGX_CONFIG}/nginx.conf ${PATH_NGX_BACKUP}/nginx.conf-$(/usr/bin/date +%F)
-  cp ${PATH_INIT_CONFIG_TEMPLATE}/nginx.conf ${PATH_NGX_CONFIG}
-  sed -i "s#/data/logs/nginx#${PATH_NGX_LOG}#g" ${PATH_NGX_CONFIG}/nginx.conf
+  # 添加启动文件
+echo "[Unit]
+Description=MySQL Server
+Documentation=man:mysqld(8)
+Documentation=http://dev.mysql.com/doc/refman/en/using-systemd.html
+After=network.target
+After=syslog.target
 
-  # 配置 demo 文件
-  mkdir ${PATH_NGX_WEB}/demo
-  echo '<h1>Welcome to nginx!</h1>' > ${PATH_NGX_WEB}/demo/index.html
-  cp ${PATH_INIT_CONFIG_TEMPLATE}/demo.conf ${PATH_NGX_CONFIG}/vhosts/
-  sed -i "s#/data/www/nginx#${PATH_NGX_WEB}#g" ${PATH_NGX_CONFIG}/vhosts/demo.conf
+[Install]
+WantedBy=multi-user.target
 
-  # 添加其它配置模板
-  cp ${PATH_INIT_CONFIG_TEMPLATE}/*.conf ${PATH_NGX_CONFIG}/templates/
+[Service]
+User=${MYSQL_PROCESS_USER}
+Group=${MYSQL_PROCESS_USER}
+ExecStart=${PATH_MYSQL_BASE_INSTALL}/bin/mysqld --defaults-file=/etc/my.cnf
+LimitNOFILE=5000" > /etc/systemd/system/mysqld.service
+
+  # 启动 MySQL
+  systemctl start mysqld
+  sleep 5
+
+  # 查看端口
+  VAR_PORT_STATUS=$(netstat -tlunp | grep ${MYSQL_PORT} | wc -l)
+  
+  if [[ ${VAR_PORT_STATUS} -ne 1 ]];then
+    FUNC_ECHO_ERROR 'MySQL 数据启动失败，请手动启动测试！'
+    exit 1012
+  fi
+
+  # 初始化用户
+  mysql -P ${MYSQL_PORT} -e "alter user root@'localhost' identified by ${MYSQL_INIT_PASSWORD};"
+
+  if [[ $? -ne 0 ]];then
+    FUNC_ECHO_ERROR 'MySQL 用户初始失败，请手动初始化！'
+    exit 1013
+  fi
 }
-
 
 #############################################################################################
 # 安装程序开始
@@ -487,8 +381,6 @@ function FUNC_INIT_NGINX() {
 FUNC_SYSTEM_CHECK
 # 用户检查
 FUNC_USER_CHECK
-# 联网检测
-FUNC_NETWORK_CHECK
 # 安装检测
 FUNC_INSTALL_CHECK
 # 安装包检测
@@ -502,14 +394,14 @@ case ${VAR_CHOICE} in
     # 系统调优
     FUNC_SYSTEM_TUNING
     sleep 2
-    # 依赖安装
-    FUNC_YUM_DEPENDS
-    sleep 2
     # 编译安装
     FUNC_INSTALL_NGINX
     sleep 2
-    # 优化配置
-    FUNC_INIT_NGINX
+    # 安装
+    FUNC_INSTALL_MYSQL
+    sleep 2
+    # 初始化
+    FUNC_INIT_MYSQL
     sleep 2
     # 输出安装信息
     FUNC_INSTALL_INFO
@@ -522,15 +414,14 @@ case ${VAR_CHOICE} in
     # 系统调优
     FUNC_SYSTEM_TUNING
     sleep 2
-    # 依赖安装
-    FUNC_YUM_DEPENDS
-    sleep 2
     # 编译安装
     FUNC_INSTALL_NGINX
     sleep 2
-    # 优化配置
-    FUNC_INIT_NGINX
+    # 安装
+    FUNC_INSTALL_MYSQL
     sleep 2
+    # 初始化
+    FUNC_INIT_MYSQL
     # 输出安装信息
     FUNC_INSTALL_INFO
 esac
---
+++ CentOS-7/mysql-57/mysql57-install-bin/packages/README.md
@@ -0,0 +1,3 @@
+# MySQL 5.7 二进制包下载地址
+
+下载地址：https://cdn.mysql.com/archives/mysql-5.7/mysql-5.7.25-linux-glibc2.12-x86_64.tar.gz
